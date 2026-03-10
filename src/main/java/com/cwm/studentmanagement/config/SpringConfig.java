b3e295ef1405df4c22c01cade26c5e456e2e78cf
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-03-10 23:44:07 +0600

    finished

--- src/main/java/com/cwm/studentmanagement/config/SpringConfig.java
+++ src/main/java/com/cwm/studentmanagement/config/SpringConfig.java
@@ -62,7 +62,7 @@ public class SpringConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://student-management-frontend-eight-lac.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
